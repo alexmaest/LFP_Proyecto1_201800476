11a8c4891d0dc49ec16dc2646bc49242a81c6a8f
alexmaest <80006030+alexmaest@users.noreply.github.com>
2021-10-05 10:00:03 -0600

    Delete [LFP]Proyecto1_201800476 directory

--- [LFP]Proyecto1_201800476/Celda.py
+++
@@ -1,6 +0,0 @@
-class Celda:
-    def __init__(self, posX, posY, boolean, hexColor):
-        self.posX = posX
-        self.posY = posY
-        self.boolean = boolean
-        self.hexColor = hexColor
